e8f27185aadbc6cf18f09d01ccc7c914819def13
Tah Ferreira <tahferreira8@gmail.com>
2025-11-07 11:58:40 -0300

    Update Exemplos - Torneio Luta.sql
    Refactor SQL queries to improve clarity and consistency.

--- Exemplos - Torneio Luta.sql
+++ Exemplos - Torneio Luta.sql
@@ -1,38 +1,42 @@
 use torneio_luta;
 
-select
+select    -- Mostrar apenas os lutadores que TÊM ACADEMIA
 	l.nome as Lutador,
 	l.estilo as Estilo,
 	a.nome as Academia,
 	a.cidade as Cidade
 from lutadores l
-inner join academias a on l.academia_id = a.id;
+inner join academias a  -- Depende do enunciado, ou seja, depende de qual tabela irá realizar a junção
+on l.academia_id = a.id;  -- O 'ON' serve de comparação, o 'JOIN' é a junção
 
 use torneio_luta;
 
-select
+select  -- Mostrar todos os lutadores, inclusive os que ainda NÃO ESTÃO EM UMA ACADEMIA
 	l.nome as Lutador,
 	l.estilo as Estilo,
 	a.nome as Academia
 from lutadores l
-left join academias a on l.academia_id = a.id;
+left join academias a -- Traz sobre a academia da esquerda chamada academias
+on l.academia_id = a.id;  -- O 'ON' serve de comparação, o 'JOIN' é a junção
 
 use torneio_luta;
 
-select
+select -- Mostrar todas as academias, mesmo as que4 ainda NÃO TÊM LUTADORES
 	l.nome as Academia,
 	a.cidade as Cidade,
 	a.nome as Lutador
-from lutadores l
-right join academias a on l.academia_id = a.id;
+from lutadores l 
+right join academias a -- Traz sobre os lutadores da direita chamada academias
+on l.academia_id = a.id;  -- O 'ON' serve de comparação, o 'JOIN' é a junção
 
 use torneio_luta;
 
-select
+select  -- Mostrar todos os lutadores e academias, com ou sem vinculo
 	l.nome as Lutador,
 	a.nome as Academia
 from lutadores l
-left join academias a on l.academia_id = a.id
+left join academias a 
+on l.academia_id = a.id  -- O 'ON' serve de comparação, o 'JOIN' é a junção
 
 union
 
@@ -40,9 +44,10 @@ select
 	l.nome as Lutador,
 	a.nome as Academia
 from lutadores l
-right join academias a on l.academia_id = a.id;
+right join academias a 
+on l.academia_id = a.id;  -- O 'ON' serve de comparação, o 'JOIN' é a junção
 
-select
+select -- Exibir quem lutou contra quem e quem venceu
 	l1.nome as Lutador_1,
     l2.nome as Lutador_2,
 	v.nome as Vencedor
@@ -53,15 +58,16 @@ inner join lutadores v on l.vencedor_id = v.id;
 
 use torneio_luta;
 
-select
-	a.nome as Academia, count(l.id) as Total_Lutadores
+select -- Liste o nome da academia e quantos lutadores ele possui
+	a.nome as Academia, count(l.id) as Total_Lutadores  -- Contar o total de lutadores
 from academias a
 left join lutadores l on a.id = l.academia_id
-group by a.nome;
+group by a.nome;  -- Grupa em nome de academias
 
 use torneio_luta;
 
-select l.nome
+select -- Liste o nome dos lutadores que NUNCA LUTARAM
+	l.nome
 from lutadores l
 left join lutas lu1 on l.id = lu1.lutador1_id
 left join lutas lu2 on l.id = lu2.lutador2_id
